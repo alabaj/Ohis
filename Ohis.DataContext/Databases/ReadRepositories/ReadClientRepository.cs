53d6173e8541090840fb9015f81e6294370ff734
Aleh Matus <a.matus@andersenlab.com>
2023-01-06 12:35:33 +0100

    updated repositories

--- Ohis.DataContext/Databases/ReadRepositories/ReadClientRepository.cs
+++ Ohis.DataContext/Databases/ReadRepositories/ReadClientRepository.cs
@@ -1,9 +1,10 @@
 ﻿using Ohis.DataContext.Databases.Base;
+using Ohis.DataContext.Databases.Interfaces;
 using Ohis.Library.Domain;
 
 namespace Ohis.DataContext.Databases.ReadRepositories
 {
-    public class ReadClientRepository : ReadRepository<ClientEntity>
+    public class ReadClientRepository : ReadRepository<ClientEntity>, IReadClientRepository
     {
     }
 }
--- Ohis.DataContext/Databases/ReadRepositories/ReadTicketRepository.cs
+++ Ohis.DataContext/Databases/ReadRepositories/ReadTicketRepository.cs
@@ -1,8 +1,9 @@
 ﻿using Ohis.DataContext.Databases.Base;
+using Ohis.DataContext.Databases.Interfaces;
 using Ohis.Library.Domain;
 
 namespace Ohis.DataContext.Databases.ReadRepositories;
 
-public class ReadTicketRepository : ReadRepository<TicketEntity>
+public class ReadTicketRepository : ReadRepository<TicketEntity>, IReadTicketRepository
 {
 }
