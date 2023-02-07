2619822dcb4ef1440dc1429924afad1448fa2680
Aleh Matus <a.matus@andersenlab.com>
2023-02-08 00:37:24 +0100

    added navigation for /clients and /clients/add

--- Ohis.MauiBlazor/MauiBlazorProgram.cs
+++ Ohis.MauiBlazor/MauiBlazorProgram.cs
@@ -1,44 +1,40 @@
-﻿using AutoMapper;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
+﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 using Ohis.DataContext;
 using Ohis.DataContext.Extensions;
-using Ohis.DataContext.Mappings;
 using Ohis.MauiBlazor.Data;
-using System.Reflection;
 
 namespace Ohis.MauiBlazor
 {
-    public static class MauiBlazorProgram
-    {
-        public static MauiApp CreateMauiApp()
-        {
-            var builder = MauiApp.CreateBuilder();
-            
-            builder
-                .UseMauiApp<App>()
-                .ConfigureFonts(fonts =>
-                {
-                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
-                });
+	public static class MauiBlazorProgram
+	{
+		public static MauiApp CreateMauiApp()
+		{
+			var builder = MauiApp.CreateBuilder();
 
-            builder.Services.AddMauiBlazorWebView();
+			builder
+				.UseMauiApp<App>()
+				.ConfigureFonts(fonts =>
+				{
+					fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+				});
+
+			builder.Services.AddMauiBlazorWebView();
 
 #if DEBUG
-            builder.Services.AddBlazorWebViewDeveloperTools();
-            builder.Logging.AddDebug();
+			builder.Services.AddBlazorWebViewDeveloperTools();
+			builder.Logging.AddDebug();
 #endif
 
-            builder.Services.AddSingleton<WeatherForecastService>();
-            builder.Services.AddDatabases();
-            builder.Services.AddServices();
-            builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+			builder.Services.AddSingleton<WeatherForecastService>();
+			builder.Services.AddDatabases();
+			builder.Services.AddServices();
+			builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
-            using AppDbContext context = new AppDbContext();
-            context.Database.Migrate();
+			using AppDbContext context = new AppDbContext();
+			context.Database.Migrate();
 
-            return builder.Build();
-        }
-    }
+			return builder.Build();
+		}
+	}
 }
